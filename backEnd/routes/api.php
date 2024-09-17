354a14e06cf86776ffd5c83babbb38afbf09d0e8
thayM <thay.contato1@gmail.com>
2024-09-17 14:56:33 -0300

    organização rotas

--- backEnd/routes/api.php
+++ backEnd/routes/api.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// cadastros
 Route::post('/cadClinica', [App\Http\Controllers\ClinicaController::class, 'store']);
-
 Route::post('/cadMedico', [App\Http\Controllers\MedicoController::class, 'store']);
 Route::post('/cadFuncionario', [App\Http\Controllers\FuncionarioController::class, 'store']);
+Route::post('/cadVacina', [App\Http\Controllers\VacinaController::class, 'store']);
