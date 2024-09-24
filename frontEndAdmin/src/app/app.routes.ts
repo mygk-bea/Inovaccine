35952d1962d5295d43963c45c4c6cf7d9094a411
mygk-bea <meyaguskobeatriz@gmail.com>
2024-09-24 15:17:05 -0300

    agrupamento de rotas angular

--- frontEndAdmin/src/app/app.routes.ts
+++ frontEndAdmin/src/app/app.routes.ts
@@ -11,35 +11,49 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'cadastro-medico',
-    loadComponent: () => import('./pages/admin/cadastro-medico/cadastro-medico.page').then( m => m.CadastroMedicoPage)
+    path:'super-user',
+    children: [
+        {
+          path: 'cadastro-medico',
+          loadComponent: () => import('./pages/admin/cadastro-medico/cadastro-medico.page').then( m => m.CadastroMedicoPage)
+        },
+        {
+          path: 'cadastro-clinica',
+          loadComponent: () => import('./pages/admin/cadastro-clinica/cadastro-clinica.page').then( m => m.CadastroClinicaPage)
+        },
+        {
+          path: 'listagem-clinicas',
+          loadComponent: () => import('./pages/admin/listagem-clinicas/listagem-clinicas.page').then( m => m.ListagemClinicasPage)
+        },
+        {
+          path: 'login',
+          loadComponent: () => import('./pages/admin/login/login.page').then( m => m.LoginPage)
+        },
+    ]
   },
   {
-    path: 'cadastro-clinica',
-    loadComponent: () => import('./pages/admin/cadastro-clinica/cadastro-clinica.page').then( m => m.CadastroClinicaPage)
-  },
-  {
-    path: 'listagem-clinicas',
-    loadComponent: () => import('./pages/admin/listagem-clinicas/listagem-clinicas.page').then( m => m.ListagemClinicasPage)
-  },
-  {
-    path: 'cadastro-funcionario',
-    loadComponent: () => import('./pages/clinica/cadastro-funcionario/cadastro-funcionario.page').then( m => m.CadastroFuncionarioPage)
-  },
-  {
-    path: 'cadastro-vacina',
-    loadComponent: () => import('./pages/clinica/cadastro-vacina/cadastro-vacina.page').then( m => m.CadastroVacinaPage)
-  },
-  {
-    path: 'listagem-funcionarios',
-    loadComponent: () => import('./pages/clinica/listagem-funcionarios/listagem-funcionarios.page').then( m => m.ListagemFuncionariosPage)
-  },
-  {
-    path: 'cadastro-lote',
-    loadComponent: () => import('./pages/clinica/cadastro-lote/cadastro-lote.page').then( m => m.CadastroLotePage)
-  },
-  {
-    path: 'listagem-vacinas',
-    loadComponent: () => import('./pages/clinica/listagem-vacinas/listagem-vacinas.page').then( m => m.ListagemVacinasPage)
-  },
+    path:'clinica',
+    children: [
+      {
+        path: 'cadastro-funcionario',
+        loadComponent: () => import('./pages/clinica/cadastro-funcionario/cadastro-funcionario.page').then( m => m.CadastroFuncionarioPage)
+      },
+      {
+        path: 'cadastro-vacina',
+        loadComponent: () => import('./pages/clinica/cadastro-vacina/cadastro-vacina.page').then( m => m.CadastroVacinaPage)
+      },
+      {
+        path: 'listagem-funcionarios',
+        loadComponent: () => import('./pages/clinica/listagem-funcionarios/listagem-funcionarios.page').then( m => m.ListagemFuncionariosPage)
+      },
+      {
+        path: 'cadastro-lote',
+        loadComponent: () => import('./pages/clinica/cadastro-lote/cadastro-lote.page').then( m => m.CadastroLotePage)
+      },
+      {
+        path: 'listagem-vacinas',
+        loadComponent: () => import('./pages/clinica/listagem-vacinas/listagem-vacinas.page').then( m => m.ListagemVacinasPage)
+      },
+    ]
+  }
 ];
