bf212790506f0b26ca73e7efb2bc464c56362770
FantesDev <vitorhenriquefantes81@gmail.com>
2024-10-14 22:53:32 -0300

    :bug: fix. ajustes de formatação

--- FrontApp/src/app/pages-app/tela-servicos/tela-servicos.page.ts
+++ FrontApp/src/app/pages-app/tela-servicos/tela-servicos.page.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCol, IonContent, IonHeader, IonRow, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { FormDiskDenunciaComponent } from 'src/app/components/forms/form-disk-denuncia/form-disk-denuncia.component';
-import { MenuComponent } from 'src/app/components/menu/menu.component';
+import { HeaderComponentComponent } from 'src/app/components/header-component/header-component.component';
 
 @Component({
   selector: 'app-tela-servicos',
   templateUrl: './tela-servicos.page.html',
   styleUrls: ['./tela-servicos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,FormDiskDenunciaComponent,MenuComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,FormDiskDenunciaComponent,HeaderComponentComponent,IonRow,IonCol]
 })
 export class TelaServicosPage implements OnInit {
 
