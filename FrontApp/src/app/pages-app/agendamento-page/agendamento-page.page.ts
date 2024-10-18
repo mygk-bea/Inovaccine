445944a2ede822f69ba6d940548202c37631da36
FantesDev <vitorhenriquefantes81@gmail.com>
2024-10-18 17:09:35 -0300

    :bug: fix. ajuste do que aparece pra mobile e pc

--- FrontApp/src/app/pages-app/agendamento-page/agendamento-page.page.ts
+++ FrontApp/src/app/pages-app/agendamento-page/agendamento-page.page.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { MenuComponent } from 'src/app/components/menu/menu.component';
 import { IonContent, IonHeader, IonTitle, IonToolbar,IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonIcon,IonList, IonModal } from '@ionic/angular/standalone';
 import { CardsAgendaComponent } from 'src/app/components/cards-agenda/cards-agenda.component';
+import { HeaderComponentComponent } from 'src/app/components/header-component/header-component.component';
+import { CardAgendaDesktopComponent } from 'src/app/components/card-agenda-desktop/card-agenda-desktop.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @Component({
   selector: 'app-agendamento-page',
   templateUrl: './agendamento-page.page.html',
   styleUrls: ['./agendamento-page.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,MenuComponent,CardsAgendaComponent,IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonIcon,IonList,IonModal]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,MenuComponent,CardsAgendaComponent,IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonIcon,IonList,IonModal,HeaderComponentComponent,CardAgendaDesktopComponent,FooterComponent]
 })
 export class AgendamentoPagePage implements OnInit {
 
