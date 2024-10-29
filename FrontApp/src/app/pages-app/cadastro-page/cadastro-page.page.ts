68070b5cdef08f44084bc9e7805c6e1150ce952c
FantesDev <vitorhenriquefantes81@gmail.com>
2024-10-29 17:52:27 -0300

    :sparkles:  feat: tela de cadastro finalizada

--- FrontApp/src/app/pages-app/cadastro-page/cadastro-page.page.ts
+++ FrontApp/src/app/pages-app/cadastro-page/cadastro-page.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastro-page',
   templateUrl: './cadastro-page.page.html',
   styleUrls: ['./cadastro-page.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ CommonModule, FormsModule,IonicModule]
 })
 export class CadastroPagePage implements OnInit {
 
