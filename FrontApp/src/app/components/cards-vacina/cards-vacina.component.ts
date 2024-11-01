8cd3e262d71d300de7beef2f510ee2c2707fef4d
vhfantes-Dev <vhfantes.dev@gmail.com>
2024-11-01 19:10:42 -0300

    :zap: att. loading para carregamento das vacinas

--- FrontApp/src/app/components/cards-vacina/cards-vacina.component.ts
+++ FrontApp/src/app/components/cards-vacina/cards-vacina.component.ts
@@ -1,34 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonIcon, IonAccordionGroup, IonItem, IonAccordion, IonRow, IonCol } from '@ionic/angular/standalone';
+import { IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonIcon, IonAccordionGroup, IonItem, IonAccordion, IonRow, IonCol, IonLoading } from '@ionic/angular/standalone';
 import { VacinaService } from 'src/app/core/services/vacina.service';
 import { Vacina } from 'src/app/core/interfaces/vacina';
 import { HttpClientModule } from '@angular/common/http';
 import { addIcons } from 'ionicons';
-import { alertCircleOutline, calendar, documentTextSharp, logOutOutline, personOutline, searchOutline } from 'ionicons/icons';
+import { alertCircleOutline, calendar, documentTextSharp, logOutOutline, personOutline, searchOutline, checkmarkOutline } from 'ionicons/icons';
 import { MenuComponent } from '../menu/menu.component';
-
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cards-vacina',
   templateUrl: './cards-vacina.component.html',
   styleUrls: ['./cards-vacina.component.scss'],
-  standalone:true,
-  imports: [CommonModule, FormsModule, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton,IonIcon,IonAccordionGroup,IonItem,IonAccordion,HttpClientModule,IonRow,IonCol,MenuComponent],
+  standalone: true,
+  imports: [IonLoading, CommonModule, FormsModule, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonButton, IonIcon, IonAccordionGroup, IonItem, IonAccordion, HttpClientModule, IonRow, IonCol, MenuComponent],
   providers: [VacinaService]
 })
-export class CardsVacinaComponent  implements OnInit {
+export class CardsVacinaComponent implements OnInit {
 
-  vacina: Vacina[] = []
-  constructor(private vacinaService: VacinaService) { 
-    addIcons({ searchOutline, calendar, documentTextSharp,logOutOutline,personOutline, alertCircleOutline });
+  vacina: Vacina[] = []; // Inicializa com um array vazio
+  isLoading: boolean = false; 
+
+  constructor(private vacinaService: VacinaService, private loadingController: LoadingController) { 
+    addIcons({searchOutline,documentTextSharp,checkmarkOutline,alertCircleOutline,calendar,logOutOutline,personOutline});
   }
 
-  ngOnInit() {
-    this.vacinaService.listarVacinas().subscribe((dados: Vacina[]) => {
-      this.vacina = dados;
+  async ngOnInit() {
+    await this.loadVacinas();
+  }
+
+  async loadVacinas() {
+    const loading = await this.loadingController.create({
+      message: 'Carregando vacinas...',
+      spinner: 'crescent',
     });
-  }
+    
+    await loading.present(); // Exibe o loader
 
+    try {
+      this.vacina = await this.vacinaService.listarVacinas().toPromise() || []; // Atribui um array vazio caso seja undefined
+    } catch (error) {
+      console.error('Erro ao carregar vacinas', error);
+    } finally {
+      await loading.dismiss(); // Esconde o loader
+    }
+  }
 }
