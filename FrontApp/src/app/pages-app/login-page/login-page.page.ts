9a29727572f91755f68bb1f244286a6c797e1d6a
vhfantes-Dev <vhfantes.dev@gmail.com>
2024-11-01 19:11:16 -0300

    :zap: att. remoção dos inputs de login para componente

--- FrontApp/src/app/pages-app/login-page/login-page.page.ts
+++ FrontApp/src/app/pages-app/login-page/login-page.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponentComponent } from 'src/app/components/header-component/header-component.component';
+import  {LoginFormComponent} from 'src/app/components/forms/login-form/login-form.component'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.page.html',
   styleUrls: ['./login-page.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, HeaderComponentComponent, IonicModule]
+  imports: [CommonModule, FormsModule, HeaderComponentComponent, IonicModule,LoginFormComponent]
 })
 export class LoginPagePage implements OnInit {
 
