00086fafa74c260251ad9eed877b34387f8e2975
vhfantes-Dev <vhfantes.dev@gmail.com>
2024-11-03 22:55:19 -0300

    :sparkles: feat. check de anonimato no disk denuncia

--- FrontApp/src/app/components/forms/form-disk-denuncia/form-disk-denuncia.component.ts
+++ FrontApp/src/app/components/forms/form-disk-denuncia/form-disk-denuncia.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { IonButton, IonCheckbox, IonCol, IonContent, IonIcon, IonInput, IonRow, ModalController } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCheckbox,
+  IonCol,
+  IonContent,
+  IonIcon,
+  IonInput,
+  IonRow,
+  ModalController, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { cloudUploadSharp, locationSharp } from 'ionicons/icons';
 import { ModalPageComponent } from '../../modals/modal/modal.component';
@@ -10,15 +18,20 @@ import { MenuComponent } from '../../menu/menu.component';
   templateUrl: './form-disk-denuncia.component.html',
   styleUrls: ['./form-disk-denuncia.component.scss'],
   standalone: true,
-  imports: [IonContent, IonInput, IonIcon, IonCheckbox, IonButton, ModalPageComponent,IonRow,IonCol,MenuComponent]
+  imports: [IonLabel, IonItem, IonContent, IonInput, IonIcon, IonCheckbox, IonButton, ModalPageComponent, IonRow, IonCol, MenuComponent]
 })
 export class FormDiskDenunciaComponent implements OnInit {
+  isAnonymous = false;
 
   constructor(private modalCtrl: ModalController) {
     addIcons({ locationSharp, cloudUploadSharp });
   }
 
   ngOnInit() {}
+
+  toggleAnonymous(event: any) {
+    this.isAnonymous = event.detail.checked;
+  }
 
   async openModal() {
     const modal = await this.modalCtrl.create({
