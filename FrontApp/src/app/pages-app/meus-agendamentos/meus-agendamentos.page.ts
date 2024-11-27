d52e4fbb014e83557c00dddd0d3404da68912f4e
FantesDev <vitorhenriquefantes81@gmail.com>
2024-11-27 16:03:37 -0300

    :sparkles: feat. modal na página de meus agendamentos

--- FrontApp/src/app/pages-app/meus-agendamentos/meus-agendamentos.page.ts
+++ FrontApp/src/app/pages-app/meus-agendamentos/meus-agendamentos.page.ts
@@ -7,27 +7,35 @@ import { AuthLoginService } from 'src/app/core/services/login/auth-login.service
 import { AgendamentoService } from 'src/app/core/services/agendamento.service';
 import { Agendamento } from 'src/app/core/interfaces/agendamento';
 import { addIcons } from 'ionicons';
-import { arrowBack, arrowBackOutline, calendarOutline, informationCircleOutline } from 'ionicons/icons';
+import { arrowBack, calendarOutline, informationCircleOutline,cashOutline,alarmOutline} from 'ionicons/icons';
 
 @Component({
   selector: 'app-meus-agendamentos',
   templateUrl: './meus-agendamentos.page.html',
   styleUrls: ['./meus-agendamentos.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule,HeaderComponentComponent,],
-  providers: [AuthLoginService,AgendamentoService]
+  imports: [IonicModule, CommonModule, HeaderComponentComponent],
+  providers: [AuthLoginService, AgendamentoService]
 })
 export class MeusAgendamentosPage implements OnInit {
   agendamentos: Agendamento[] = [];
-  constructor(private navCtrl: NavController, private animationCtrl: AnimationController,private authService: AuthLoginService,
-    private agendamentoService: AgendamentoService,) {
-      addIcons({ informationCircleOutline,arrowBack,calendarOutline });
-     }
+  selectedAgendamento: Agendamento | null = null;  // Armazena o agendamento selecionado
+  isModalOpen: boolean = false;  // Controla a visibilidade do modal
+  pacienteId: string = this.authService.getUserId();
+
+  constructor(
+    private navCtrl: NavController,
+    private animationCtrl: AnimationController,
+    private authService: AuthLoginService,
+    private agendamentoService: AgendamentoService
+  ) {
+    addIcons({ informationCircleOutline, arrowBack, calendarOutline,cashOutline,alarmOutline });
+  }
 
   ngOnInit() {
     this.carregarAgendamentos();
   }
-  pacienteId: string = this.authService.getUserId();
+
   carregarAgendamentos() {
     this.agendamentoService.listarAgendamento(+this.pacienteId).subscribe(
       (data) => {
@@ -38,6 +46,19 @@ export class MeusAgendamentosPage implements OnInit {
       }
     );
   }
+
+  // Abre o modal com os detalhes do agendamento
+  openModal(agendamento: Agendamento) {
+    this.selectedAgendamento = agendamento;  // Define o agendamento selecionado
+    this.isModalOpen = true;  // Abre o modal
+  }
+
+  // Fecha o modal
+  closeModal() {
+    this.isModalOpen = false;  // Fecha o modal
+  }
+
+  // Método para navegação
   navigateTo(path: string, direction: 'back' | 'forward') {
     const animation = direction === 'back' ? this.createBackwardAnimation() : undefined;
     if (direction === 'back') {
@@ -47,6 +68,7 @@ export class MeusAgendamentosPage implements OnInit {
     }
   }
 
+  // Animação para navegação "back"
   createBackwardAnimation() {
     return (baseEl: any, opts?: any) => {
       const enterAnimation = this.animationCtrl.create()
